fa298ac4b60f842d30569a3f07e029bfd9bc2d64
PIRATtt <77304210+PIRATtt@users.noreply.github.com>
2022-11-24 18:09:06 +0300

    Update 12.py
    комит для проверка ВМ

--- Tasks/12.py
+++ Tasks/12.py
@@ -91,49 +91,49 @@ if __name__ == '__main__':
 # Создать функцию print_ip_table, которая отображает таблицу доступных и недоступных IP-адресов.
 
 # Функция ожидает как аргументы два списка:
-import subprocess
-from tabulate import tabulate
+# import subprocess
+# from tabulate import tabulate
 
-ip=['1.1.1.1', '5.5.5.5', '8.8.8.2-6', '10.10.10.5-10.10.10.10']
-def sort_ip(ip_list):
-    ip=ip_list
-    dell=[]
-    for i in ip:
-        if '-' in i:
-            dell.append(i)
-            i2=i.split('-')
-            if '.' in i2[1]:
-                if int(i2[0].split('.')[3])<int(i2[1].split('.')[3]):
-                    for num in list(range(int(i2[0].split('.')[3]), int(i2[1].split('.')[3])+1)):
-                        i2[0]=i2[0].replace('{}'.format(i2[0].split('.')[3]), str(num))
-                        ip.append(i2[0])
-                else:
-                    print('ERROR!!! Не правильно ввели ip!')
-            else:
-                if i2[1].isdigit()==True:
-                    if int(i2[0].split('.')[3])<int(i2[1]):
-                        for num in list(range(int(i2[0].split('.')[3])-1, int(i2[1])+1)):
-                            i2[0]=i2[0].replace('{}'.format(i2[0].split('.')[3]), str(num))
-                            ip.append(i2[0])
-                else:
-                    print('ERROR!!! Не правильно ввели ip!')
-    for d in dell:
-        ip.remove(d)
-    return ip
+# ip=['1.1.1.1', '5.5.5.5', '8.8.8.2-6', '10.10.10.5-10.10.10.10']
+# def sort_ip(ip_list):
+#     ip=ip_list
+#     dell=[]
+#     for i in ip:
+#         if '-' in i:
+#             dell.append(i)
+#             i2=i.split('-')
+#             if '.' in i2[1]:
+#                 if int(i2[0].split('.')[3])<int(i2[1].split('.')[3]):
+#                     for num in list(range(int(i2[0].split('.')[3]), int(i2[1].split('.')[3])+1)):
+#                         i2[0]=i2[0].replace('{}'.format(i2[0].split('.')[3]), str(num))
+#                         ip.append(i2[0])
+#                 else:
+#                     print('ERROR!!! Не правильно ввели ip!')
+#             else:
+#                 if i2[1].isdigit()==True:
+#                     if int(i2[0].split('.')[3])<int(i2[1]):
+#                         for num in list(range(int(i2[0].split('.')[3])-1, int(i2[1])+1)):
+#                             i2[0]=i2[0].replace('{}'.format(i2[0].split('.')[3]), str(num))
+#                             ip.append(i2[0])
+#                 else:
+#                     print('ERROR!!! Не правильно ввели ip!')
+#     for d in dell:
+#         ip.remove(d)
+#     return ip
 
-def print_ip_table(ip_list):
-    ip=sort_ip(ip_list)
-    good=[]
-    bad=[]
-    for ip in ip_list:
-        result =subprocess.run(['ping', '-n', '1', ip], stdout=subprocess.DEVNULL, encoding="utf-8")
-        if result.returncode == 0:
-            good.append(ip)
-        else:
-            bad.append(ip)
-    all_ip={"Reachable": good, "Unreachable": bad}
-    result=tabulate(all_ip, headers='keys')
-    return result
+# def print_ip_table(ip_list):
+#     ip=sort_ip(ip_list)
+#     good=[]
+#     bad=[]
+#     for ip in ip_list:
+#         result =subprocess.run(['ping', '-n', '1', ip], stdout=subprocess.DEVNULL, encoding="utf-8")
+#         if result.returncode == 0:
+#             good.append(ip)
+#         else:
+#             bad.append(ip)
+#     all_ip={"Reachable": good, "Unreachable": bad}
+#     result=tabulate(all_ip, headers='keys')
+#     return result
 
-if __name__ == '__main__':
-    print(print_ip_table(ip))
+# if __name__ == '__main__':
+#     print(print_ip_table(ip))
